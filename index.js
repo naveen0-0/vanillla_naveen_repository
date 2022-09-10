d03fd91eac280c7e653e8bb37354e5f97ab1441a
naveen0-0 <baddigamnaveenkumarreddy1910@gmail.com>
2022-09-10 18:30:18 +0530

    Form Submit Functionality Added

---
+++ index.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('form')
+form.addEventListener('submit', sendMail)
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const subject = document.getElementById('subject')
+const message = document.getElementById('message')
+const success = document.getElementById('success')
+const error = document.getElementById('error')
+const submittext = document.getElementById('submittext')
+const spinner = document.getElementById('loader')
+const submitbtn =  document.getElementById("formSubmit")
+
+function sendMail(e){
+  e.preventDefault()
+  spinner.style.display = "block"
+  submittext.style.display = "none"
+  submitbtn.setAttribute("disabled", true)
+  const template = { 
+    username : username.value, 
+    email : username.value, 
+    subject:subject.value, 
+    message:subject.value 
+  }
+  emailjs.send("service_yvsd6pm", "template_zc080aa", template, "p4D5uyCTOzuEE75ss")
+    .then(() => {
+      username.value = ""
+      email.value = ""
+      subject.value = ""
+      message.value = ""
+      success.classList.add('displaymsg')
+      setTimeout(() => {
+        success.classList.remove('displaymsg')
+      },5000)
+    })
+    .catch(() => {
+      error.classList.add('displaymsg')
+      setTimeout(() => {
+        error.classList.remove('displaymsg')
+      },5000)
+    }).finally(() => {
+        spinner.style.display = "none"
+        submittext.style.display = "block"
+        submitbtn.removeAttribute('disabled')
+    })
+}
